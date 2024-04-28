b9306fa38fefb6f989dc3e94c51e8148b2ce9a12
Desktop <felipemeneghettib23@gmail.com>
2024-04-28 10:56:46 -0300

    Funções quase prontas

--- index.js
+++ index.js
@@ -7,20 +7,38 @@ const app= express(); // sera criada a aplicação utilizando o express citado a
 
 //requisiçao vem da internet
 //A resposta é enviada para a Internet para quem fez a requisiçao 
-function retornarPaginaInicial(requisição, resposta){
-    resposta.write('<!DOCTYPE html>');
-    resposta.write('<head>');
-    resposta.write('<meta charset=8"utf-8">');
-    resposta.write('<title>Tabuada</title>');
-    resposta.write('</head>');
-    resposta.write('<body>');
-    resposta.write('<h3>Pagina Inicial</h3>');
-    resposta.write('</body>');
-    resposta.write('</html>');
-    resposta.end();
-};
+
+function CalcularTabuada(requisicao,resposta){
+//extrair da url o numero que sera usado para a tabuada e a quantidade de vezes que vai ser executada a tabuada.
+let numero= parseInt(requisicao.query.numero);
+let sequencia= parseInt(requisicao.query.sequencia);
+if(!sequencia){
+    sequencia=10;
+}
+
+resposta.write('<!DOCTYPE hmtl>');
+resposta.write('<html>');
+resposta.write('<head>');
+resposta.write('meta charset="utf-8">');
+resposta.write('<title>Tabuada</title>');
+resposta.write("</head>");
+resposta.write('<body>');
+if(numero){
+    for(let i=0; i <= sequencia; i++){
+        const resultado = (numero * i);
+        resposta.write('<h1>' + numero + 'x' + i + '=' + resultado + '</h1>');
+    }
+}
+resposta.write('</body>');
+resposta.end();
+
+
+}
+
+
+
 //Cuidado JS permite que funções sejam passadas como parametros para outras funções.
-app.get("/", retornarPaginaInicial);
+app.get("/tabuada", CalcularTabuada);
 // ( ) =>{}  é uma função anonima conhecida como arrow function.
 app.listen(porta, host, () => {
     console.log("Servidor esta executando em http://" + host + ":" + porta);
